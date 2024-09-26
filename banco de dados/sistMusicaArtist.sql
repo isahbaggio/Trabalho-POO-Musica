e63048a1d2d1c4e0e1475b0fb67ee2a14d40ce6c
Isabel Baggio <130410760+isahbaggio@users.noreply.github.com>
2024-09-26 19:13:18 -0300

    Update sistMusicaArtist.sql

--- banco de dados/sistMusicaArtist.sql
+++ banco de dados/sistMusicaArtist.sql
@@ -16,13 +16,13 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `artista`
+-- Table structure for table `artist`
 --
 
-DROP TABLE IF EXISTS `artista`;
+DROP TABLE IF EXISTS `artist`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `artista` (
+CREATE TABLE `artist` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -30,12 +30,12 @@ CREATE TABLE `artista` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `artista`
+-- Dumping data for table `artist`
 --
 
-LOCK TABLES `artista` WRITE;
-/*!40000 ALTER TABLE `artista` DISABLE KEYS */;
-/*!40000 ALTER TABLE `artista` ENABLE KEYS */;
+LOCK TABLES `artist` WRITE;
+/*!40000 ALTER TABLE `artist` DISABLE KEYS */;
+/*!40000 ALTER TABLE `artist` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
