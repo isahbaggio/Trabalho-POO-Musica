351e31200045b13a06f967a1579dae89fd1fae48
Isabel Baggio <130410760+isahbaggio@users.noreply.github.com>
2024-09-26 16:29:56 -0300

    Create SistMusicApplicationTests.java

---
+++ sistMusica/src/main/test/java/br/edu/univille/poo/sistMusica/SistMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.univille.poo.sistMusica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
