7287c8b4243327f94270bf382b8479c24bc4b2a1
Isabel Baggio <130410760+isahbaggio@users.noreply.github.com>
2024-09-26 16:25:04 -0300

    Create SistMusicApplication.java

---
+++ sistMusica/src/main/java/br/edu/univille/poo/sistMusica/SistMusicApplication.java
@@ -0,0 +1,13 @@
+package br.edu.univille.poo.sistema_musica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistMusicApplication.class, args);
+	}
+
+}
